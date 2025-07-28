6931ad865723c87423956212e36b3cc726f84a03
Anushika06 <anushika.24bcs10344@sst.scaler.com>
2025-07-28 21:37:42 +0530

    Update DatabaseConnection.java

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String url = "jdbc:mysql://127.0.0.1:3306/airdb";
-    private static final String username = "YOUR_USERNAME";
-    private static final String password = "YOUR_PASSWORD";  
+    private static final String username = "root";
+    private static final String password = "Mn@h_2006";  
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(url, username, password);
